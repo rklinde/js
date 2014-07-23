505039a8303ba731724e66088fb82aa6e4d94a5f
Rebecca Linde <rklinde@gmail.com>
2014-07-23 15:45:28 -0400

    changes to scripts

--- Scripts/script.js
+++ Scripts/script.js
@@ -22,3 +22,49 @@ console.log(fav[1][0]);
 console.log(fav[1][2]);
 console.log(fav[0][4]);
 
+//Exercise 4
+var myVar = 15;
+
+if(myVar < 10){
+	console.log("The variable is less than 10");
+}else{
+	console.log("The variable is" + " " + myVar + " and it is greater than 10");
+} 
+
+//Function Examples
+
+//Example 1
+function addNumber(x) {
+	return someNumber + 2;
+}
+
+var someNumber = 7;
+console.log(addNumber(2))
+//
+
+//Example 2
+function createName() {
+	var fname = "Rebecca";
+	var lname = "Linde";
+	console.log(fname + " " + lname);
+}
+
+createName()
+//
+
+//Example 3
+function name(fname) {
+	return alert(fname);
+}
+
+name("Rebecca");
+//
+
+//Example 4
+function door(prize) {
+	console.log("your prize is" + " " + prize);
+}
+
+door("money");
+door("fame");
+door("nothing");
