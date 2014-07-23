ae5726271392d5ad009b83e98c6aed3bebf38c16
Rebecca Linde <rklinde@gmail.com>
2014-07-22 20:46:11 -0400

    First three exercises added

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,24 @@
+/*
 alert("Hello World");
+*/
+
+//Exercise 1
+var x = 5;
+var y =10;
+var res = x + y;
+console.log(res);
+
+//Excercise 2
+var fname = "Rebecca";
+var lname = "Linde";
+var fullName = fname + " " + lname;
+console.log(fullName);
+
+//Exercise 3
+var channels = ["HBO", "NBC", "FOX", "FX", "SHO"]; //"HBO"
+var shows = ["Veep", "Parks & Rec", "Mindy Proj", "Louie", "Masters of Sex"]; //"Parks & Rec"
+var fav = [channels, shows];
+console.log(fav[1][0]);
+console.log(fav[1][2]);
+console.log(fav[0][4]);
+
